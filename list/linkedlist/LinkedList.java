499933aee91f8d3a77bf0d89afbdca67c02ed3a1
Naveen Kumar Gupta (CS) <propnaveen@gmail.com>
2018-07-29 07:43:36 +0530

    Trial Commit

--- list/linkedlist/LinkedList.java
+++ list/linkedlist/LinkedList.java
@@ -1,4 +1,42 @@
 
 
 public class LinkedList<T> {
+
+	private Node<T> head;
+
+	public class Node<T> {
+		Node<T> next;
+		T data;
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+	
+	public void addFirst(T data) {
+		Node<T> node = new Node<>(data);
+		if (head == null) {
+			head = node;
+			return;
+		}
+		node.next = head;
+		head = node;
+	}
+
+	public void print() {
+		print(head);
+	}
+	private void print(Node<T> node) {
+		Node<T> current = head;
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		LinkedList<String> list = new LinkedList<>();
+		list.addFirst("Admin");
+		list.print();
+	}
 }
